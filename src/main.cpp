c4891b5afc3004551be3fc8129d51dbfa341f38d
Johevin Blesstowi <BS22-1@slc.net>
2023-10-26 13:09:32 +0700

    finish logic

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,48 @@
 #include <Arduino.h>
+#include <Servo.h>
 
-int myFunction(int, int);
+#define PIN_TRIGGER 9
+#define PIN_ECHO 8
+#define PIN_SERVO 10
+
+Servo myservo;
+int pos = 0;
 
 void setup() {
-  Serial.begin(115200);
+   pinMode(PIN_TRIGGER, OUTPUT);
+   pinMode(PIN_ECHO, INPUT);
+   Serial.begin(115200);
+   myservo.attach(PIN_SERVO);
 }
 
-void loop() {
-  Serial.println("test");
-  Serial.println("ting");
+int getDistance() {
+   digitalWrite(PIN_TRIGGER,LOW);
+   delayMicroseconds(2);
+
+   digitalWrite(PIN_TRIGGER, HIGH);
+   delayMicroseconds(10);
+
+   digitalWrite(PIN_TRIGGER, LOW);
+   int pulse = pulseIn(PIN_ECHO, HIGH);
+   return pulse * 0.034 / 2;
 }
+
+void gateController(int cm, int timeDelay) {
+   if(cm < 15) {
+      myservo.write(180);
+      delay(timeDelay);
+   } else {
+      myservo.write(0);
+      delay(timeDelay);
+   }
+
+}
+
+void loop(){
+
+   int cm = getDistance();
+   gateController(cm, 2000);
+   Serial.print("Jarak (cm) : ");
+   Serial.println(cm);
+
+} 
