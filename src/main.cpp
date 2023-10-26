b0f5e28e5d0b1662b44cf2ea8ece1d9cbe87c339
johe at hd3 <johe@hd3.com>
2023-10-26 14:49:27 +0700

    firebase madness

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,26 @@
 #include <controller/distance.h>
 #include <controller/gate.h>
 
+// #include <Firebase.h>
+
+// //Provide the token generation process info.
+// #include "addons/TokenHelper.h"
+// //Provide the RTDB payload printing info and other helper functions.
+// #include "addons/RTDBHelper.h"
+
+// // Insert your network credentials
+// #define WIFI_SSID "REPLACE_WITH_YOUR_SSID"
+// #define WIFI_PASSWORD "REPLACE_WITH_YOUR_PASSWORD"
+
+// // Insert Firebase project API Key
+// #define API_KEY "REPLACE_WITH_YOUR_FIREBASE_PROJECT_API_KEY"
+
+// // Insert RTDB URLefine the RTDB URL */
+// #define DATABASE_URL "REPLACE_WITH_YOUR_FIREBASE_DATABASE_URL" 
+
+// //Define Firebase Data object
+// FirebaseData fbdo;
+
 Servo myservo;
 
 void setup() {
