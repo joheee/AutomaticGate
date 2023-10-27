b148da43c6d77df3bc5677c1fd7ce29d6b0039e8
Johevin Blesstowi <BS22-1@slc.net>
2023-10-27 14:11:27 +0700

    firebase setup

--- src/main.cpp
+++ src/main.cpp
@@ -4,35 +4,65 @@
 #include <controller/distance.h>
 #include <controller/gate.h>
 #include <Firebase.h>
-
-//Provide the token generation process info.
 #include "addons/TokenHelper.h"
-//Provide the RTDB payload printing info and other helper functions.
 #include "addons/RTDBHelper.h"
-
-// Insert your network credentials
-#define WIFI_SSID "REPLACE_WITH_YOUR_SSID"
-#define WIFI_PASSWORD "REPLACE_WITH_YOUR_PASSWORD"
-
-// Insert Firebase project API Key
-#define API_KEY "REPLACE_WITH_YOUR_FIREBASE_PROJECT_API_KEY"
-// Insert RTDB URLefine the RTDB URL */
-#define DATABASE_URL "REPLACE_WITH_YOUR_FIREBASE_DATABASE_URL" 
+#include <ESP8266WiFi.h>
 
 FirebaseData fbdo;
+FirebaseAuth auth;
+FirebaseConfig config;
 Servo myservo;
 
 void setup() {
+   //  initialize pin
    pinMode(PIN_TRIGGER, OUTPUT);
    pinMode(PIN_ECHO, INPUT);
    myservo.attach(PIN_SERVO);
    Serial.begin(74880);
+
+   // connect wifi
+   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+   Serial.print("Connecting to Wi-Fi");
+   while (WiFi.status() != WL_CONNECTED){
+      Serial.print(".");
+      delay(300);
+   }
+   Serial.println();
+   Serial.print("Connected with IP: ");
+   Serial.println(WiFi.localIP());
+   Serial.println();
+
+   // assign firebase cred
+   config.api_key = API_KEY;
+   config.database_url = DATABASE_URL;
+   auth.user.email = USER_EMAIL;
+   auth.user.password = USER_PASSWORD;
+   config.token_status_callback = tokenStatusCallback;
+   Firebase.begin(&config, &auth);
+   Firebase.reconnectWiFi(true);
 }
 
+int count = 0;
+
 void loop(){
+   if (Firebase.ready()){
+      if (Firebase.RTDB.setInt(&fbdo, "test/int", count)){
+         Serial.println("PASSED");
+         Serial.println("PATH: " + fbdo.dataPath());
+         Serial.println("TYPE: " + fbdo.dataType());
+      }
+      else {
+         Serial.println("FAILED");
+         Serial.println("REASON: " + fbdo.errorReason());
+      }
+      count++;
+   }
+
    int cm = getDistance();
    bool isOpen = isGateOpened(myservo, cm, 2000);
 
+   Serial.print("Firebase Status : ");
+   Serial.println(Firebase.ready());
    Serial.print("Jarak (cm) : ");
    Serial.println(cm);
    Serial.print("Gate Kebuka : ");
