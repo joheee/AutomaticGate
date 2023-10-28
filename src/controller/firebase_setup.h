f1af848f8f97c73beba6f95ebdd39823c072e481
Johevin Blesstowi <BS22-1@slc.net>
2023-10-28 15:58:59 +0700

    final result

--- src/controller/firebase_setup.h
+++ src/controller/firebase_setup.h
@@ -3,12 +3,12 @@
 #include "addons/TokenHelper.h"
 #include "addons/RTDBHelper.h"
 
-void firebaseSetupInit(FirebaseData fbdo, FirebaseAuth auth, FirebaseConfig config){
-    config.api_key = API_KEY;
-    config.database_url = DATABASE_URL;
-    auth.user.email = USER_EMAIL;
-    auth.user.password = USER_PASSWORD;
-    config.token_status_callback = tokenStatusCallback;
-    Firebase.begin(&config, &auth);
+void firebaseSetupInit(FirebaseData *fbdo, FirebaseAuth *auth, FirebaseConfig *config){
+    config->api_key = API_KEY;
+    config->database_url = DATABASE_URL;
+    auth->user.email = USER_EMAIL;
+    auth->user.password = USER_PASSWORD;
+    config->token_status_callback = tokenStatusCallback;
+    Firebase.begin(config, auth);
     Firebase.reconnectWiFi(true);
 }
